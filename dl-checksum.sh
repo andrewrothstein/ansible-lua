8df387707c8f34cd54cf26363afe865d0a65b523
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-12-03 13:58:43 -0500

    lua:5.4.1 latest dep roles DRY + logging + smoke test

---
+++ dl-checksum.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://www.lua.org/ftp
+
+dl()
+{
+    local ver=$1
+    local file="lua-${ver}.tar.gz"
+    local url=$MIRROR/$file
+    local lfile=$DIR/$file
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s': sha256:%s\n" $ver `sha256sum $lfile | awk '{print $1}'`
+}
+
+dl ${1:-5.4.1}
